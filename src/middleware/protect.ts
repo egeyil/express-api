bd7029c3eed6fede9bf98669d3a65a7e34271d4c
Ege Yıldırım <egeyildirimdevelopment@gmail.com>
2023-01-03 22:34:42 +0300

    Update protect.ts, refactor and simplify both protect.ts and jwt.utils.ts

---
+++ src/middleware/protect.ts
@@ -0,0 +1,57 @@
+import {Request, Response, NextFunction} from "express";
+import {issueAccessToken, signJwt, verifyJwt} from "../utils/jwt.utils";
+import {accessTokenCookieOptions, accessTokenName, refreshTokenName} from "../config/globalVariables";
+import jwt, {JwtPayload, VerifyErrors} from "jsonwebtoken";
+
+// This middleware is used to protect routes, deserialize the user from the JWT, and attach it to the res.locals object
+const protect = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const accessToken = req.cookies[accessTokenName];
+    const refreshToken = req.cookies[refreshTokenName];
+
+    if (!accessToken && !refreshToken) {
+      return res.status(401).json({message: "Unauthorized"});
+    }
+
+    const refreshTokenCallback = (err: VerifyErrors | null, decoded: JwtPayload | string | undefined) => {
+      try {
+        if (err) {
+          return res.status(401).json({message: "Unauthorized"});
+        }
+        const newAccessToken = issueAccessToken(decoded as JwtPayload);
+        res.cookie(accessTokenName, newAccessToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: 'none',
+          maxAge: 60 * 60 * 1000 // 1 hour
+        });
+        res.locals.user = decoded;
+        return next();
+      } catch (e) {
+        return res.status(500).json({message: "Internal server error"});
+      }
+    }
+    const accessTokenCallback = (err: VerifyErrors | null, decoded: JwtPayload | string | undefined) => {
+      try {
+        if (err) {
+          console.log(err);
+          if (err.name === "TokenExpiredError") {
+            // Verify refreshToken
+            jwt.verify(refreshToken, refreshTokenName, refreshTokenCallback);
+          }
+          return res.status(401).json({message: "Unauthorized"});
+        }
+        res.locals.user = decoded;
+        return next();
+      } catch (e) {
+        return res.status(500).json({message: "Internal server error"});
+      }
+    }
+
+    jwt.verify(accessToken, accessTokenName, accessTokenCallback);
+  } catch (e) {
+    return res.status(401).json({message: "Unauthorized"});
+  }
+};
+
+export default protect;
--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -3,57 +3,26 @@ import {accessTokenName, refreshTokenName} from "../config/globalVariables";
 
 // Create secrets with require('crypto').randomBytes(64).toString('hex')
 
-export function signJwt(object: Object, keyName: string, options?: jwt.SignOptions | undefined) {
-  try {
-    const signingKey = Buffer.from(
-      keyName,
-      "base64"
-    ).toString("ascii");
-
-    return jwt.sign(object, signingKey, {
-      ...(options && options),
-    });
-  } catch (e: any) {
-    console.log(e);
-    return undefined;
-  }
+export function signJwt(object: Object, secret: string, options?: jwt.SignOptions | undefined) {
+  return jwt.sign(object, secret, {
+    ...(options && options),
+  });
 }
 
-export function verifyJwt(token: string, keyName: string, options?: jwt.VerifyOptions | undefined) {
-  try {
-    const publicKey = Buffer.from(keyName, "base64").toString(
-      "ascii"
-    );
-    const decoded = jwt.verify(token, publicKey);
-    return {
-      valid: true,
-      expired: false,
-      decoded,
-    };
-  } catch (e: any) {
-    console.error(e);
-    return {
-      valid: e.name !== "TokenExpiredError",
-      expired: e.name === "TokenExpiredError",
-      decoded: undefined,
-    };
-  }
+export function verifyJwt(token: string, secret: string, options?: jwt.VerifyOptions | undefined) {
+  return jwt.verify(token, secret);
 }
 
 export function issueAccessToken(user: JwtPayload) {
   return signJwt({
-    UserInfo: {
-      username: user.username,
-      email: user.email,
-      roles: user.roles && Object.values(user.roles).filter(Boolean),
-    },
+    username: user.username,
+    email: user.email,
+    roles: user.roles && Object.values(user.roles).filter(Boolean),
   }, accessTokenName, {expiresIn: "15m"})
 }
 
 export function issueRefreshToken(user: JwtPayload) {
   return signJwt({
-    UserInfo: {
-      username: user.username,
-    },
+    username: user.username,
   }, accessTokenName, {expiresIn: "90d"})
 }
