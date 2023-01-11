48102abd4918d3d4888543afc1380240e0c9b512
Ege Yıldırım <egeyildirimdevelopment@gmail.com>
2023-01-11 17:17:58 +0300

    Minor cors update

--- src/utils/corsOptions.ts
+++ src/utils/corsOptions.ts
@@ -4,9 +4,12 @@ import {CorsOptions} from "cors";
 const corsOptions: CorsOptions  = {
   origin: (origin, callback)=> {
     // Remove !origin if you do not want to allow requests with no origin (Server-to-server requests or local tools)
-    if (origin && allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (origin && allowedOrigins.indexOf(origin) !== -1) {
       return callback(null, true)
-    } else {
+    } else if (process.env.NODE_ENV === 'dev' && !origin) {
+      return callback(null, true)
+    }
+    else {
       return callback(new Error('Not allowed by CORS'));
     }
   },
